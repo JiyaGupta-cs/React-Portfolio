89f9bc9845c8891f0761c0dd057979cb2d4dcc68
Jiya <coderaholic21@gmail.com>
2024-02-20 00:02:59 +0530

    feat: Created Portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,188 @@
+import ShopNexImg from '../images/shopnex.png';
+import CapabilixImg from '../images/capabilix.png';
+export const Bio = {
+  name: "Jiya Gupta",
+  roles: [
+    "Front End Developer",
+    "Programmer",
+  ],
+  description:
+    "I am an Open Source Enthusiast and Agile Learner, always eager to take on new challenges. Passionate about contributing to innovative collaborative projects, I actively engage in the open-source community. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/JiyaGupta-cs",
+  resume:
+    "",
+  linkedin: "linkedin.com/in/jiya-gupta-engineeringstudent/",
+  twitter: "https://twitter.com/JiyaGupta_dev",
+  mail: "mailto:coderaholic21@gmail.com",
+  github: "https://github.com/JiyaGupta-cs",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },      
+      {
+          name: "Tailwind",
+          image:
+            "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+        },
+        {
+          name: "Firebase",
+          image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        },
+        {
+          name: "Git",
+          image:
+            "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://static-00.iconduck.com/assets.00/github-icon-2048x1998-3j08tfre.png",
+        },
+    ],
+  }
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.hacksquad.dev/favicon/favicon-256x256.png",
+    role: "HackSquad 2023",
+    company: "A month-long Open-Source Contest",
+    date: "October 2023",
+    desc: "Winner of HackSquad among 2000+ teams. Submitted impactful Pull Requests to diverse     GitHub projects, showcasing proficiency in collaborative coding and issue resolution.",
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Screenshot%20from%202024-01-25%2022-38-31.png?alt=media&token=2785903f-1a4e-41f5-afd2-6adcfe56d058",
+  },
+  {
+    id: 1,
+    img: "https://media.dev.to/cdn-cgi/image/width=320,height=320,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Forganization%2Fprofile_image%2F7504%2Fe7cb408f-06f0-40b2-9281-886bded91961.png",
+    role: "FormTribe Hackathon",
+    company: "Formbricks",
+    date: "October 2023",
+    desc: "Contributed to Formbricks, a real-world product, addressing and resolving issues in a live Github environment. Contributed to the enhancement of multiple open-source projects.",    
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/1696514649120.jpeg?alt=media&token=e7f6757b-edfa-4138-a692-d6709eeef3e2",
+  },
+  {
+    id: 2,
+    img: "https://iwoc.codes/public/img/iwoc_logo.png",
+    role: "IWOC 2024",
+    company: "Innogeeks",
+    date: "January-February 2024",
+    desc: "Secured 3rd position among 500+ registrations. Demonstrated collaborative coding    expertise through impactful contributions to GitHub projects.",    
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+  },
+  {
+    id: 3,
+    img: "https://www.jwoc.tech/_next/static/media/jwoc-2024.652c49b8.svg",
+    role: "JWOC 2024",
+    company: "JGEC",
+    date: "January-February 2024",
+    desc: "Advanced from a mentee role to that of a mentor, actively participated in the review and merging of PRs, contributing significantly to the growth and development of the projects.",
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/1691181448873.jpeg?alt=media&token=ee85eb8f-7247-43cd-9a1d-ce9f58ea62a6",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://yt3.googleusercontent.com/ytc/AIf8zZQEPKKWupk3e0TA08I3i7V2hjdD-TaQvGIeN8M9DA=s900-c-k-c0x00ffffff-no-rj",
+    school: "KIET Group Of Instituitions, Ghaziabad",
+    degree: "Bachelor of Technology - BTech, Computer Science",
+    date: "Jul 2023 - Jul 2027",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science at KIET Group Of Instituitions, Ghaziabad. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    
+  },
+  {
+    id: 1,
+    img: "https://seeklogo.com/images/C/cbse-logo-46D5A6B556-seeklogo.com.png",
+    school: "Central Board of Secondary Education",
+    date: "Apr 2023",
+    grade: "88%",
+    desc: "I completed my class 12 high school education in 2023, where I studied Science with Computer Science.",
+    degree: "(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://cisceboard.org/images/cisce-60-logo-new.png",
+    school: "Indian Certificate of Secondary Education",
+    date: "Apr 2021",
+    grade: "98%",
+    desc: "I completed my class 10 education in 2021, where I studied Science with Computer Science.",
+    degree: "(X), Science with Computer",
+  },
+];
+
+export const projects = [
+   {
+    id: 0,
+    title: "Capabilix",
+    description:
+      "It is a social media platform with a focus on fostering a supportive community for disabled individuals that can provide a space to share their experiences, support one another, promote events, grab a job, and become self-dependent.",
+    image: CapabilixImg,
+    tags: ["React Js", "Firebase", "Google Calendar", "Twitter API"],
+    category: "web app",
+    github: "https://github.com/JiyaGupta-cs/mytwit",
+    webapp: "https://mytwit.vercel.app/",
+    member: [
+      {
+        name: "Jiya Gupta",
+        img: "https://avatars.githubusercontent.com/u/140608790?v=4",
+        linkedin: Bio.linkedin,
+        github: Bio.github,
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "ShopNex",
+    description:
+      "Shopnex is your one-stop destination for fashionable clothing for the entire family. Discover a wide selection of men's, women's, and kids' apparel, ensuring that everyone can find the perfect style and fit. Shop with confidence and convenience at Shopnex for all your clothing needs.",
+    image:ShopNexImg,
+    tags: ["HTML", "CSS","JavaScript","React JS"],
+    category: "machine learning",
+    github: "https://github.com/JiyaGupta-cs/ShopNex",
+    webapp: "https://shopnex.vercel.app/",
+    member: [
+      {
+        name: "Jiya Gupta",
+        img: "https://avatars.githubusercontent.com/u/140608790?v=4",
+        linkedin: Bio.linkedin,
+        github: Bio.github,
+      },
+    ],
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
